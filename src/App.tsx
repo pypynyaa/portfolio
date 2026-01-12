9083e7ce4adfd4ab73475d80fb52da30539fb991
pypynyaa <nikita662006@bk.ru>
2026-01-12 04:11:12 +0300

    Fix github actions path

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 // Импортируйте ваш провайдер (проверьте путь!)
 import { LanguageProvider } from "@/contexts/LanguageContext"; 
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import { HashRouter, Routes, Route } from 'react-router-dom';
 
 const queryClient = new QueryClient();
 
@@ -17,12 +17,12 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <HashRouter>
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </TooltipProvider>
     </LanguageProvider>
   </QueryClientProvider>
